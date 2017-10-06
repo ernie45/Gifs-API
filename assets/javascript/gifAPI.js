14379574c82a20cd20b5246c84eb0039152e25a5
Ernesto Hernandez <Ernesto Hernandez>
2017-10-05 17:42:21 -0700

    added comments to guide coding process

--- assets/javascript/gifAPI.js
+++ assets/javascript/gifAPI.js
@@ -1,5 +1,6 @@
-		//ORIGINAL BUTTONS
+//MAIN FUNCTION////////////////////////////////////////////////////////////////////////////////////////////////////		
 	$(document).ready(function(){
+		//ORIGINAL BUTTONS
 		var topics = ["fix", "fix", "fix", "fix", "fix", "fix"];
 		//SHOW THE BUTTONS
 		displayButtons();
@@ -10,18 +11,23 @@
 			topics.push(subject);
 			displayButtons();
 		});
-		//CLICKING ON A BUTTON SEARCHES FOR THAT 
+		//CLICKING ON A BUTTON SEARCHES FOR THAT SUBJECT
 		$("#buttonsGoHere").on("click", ".subject", function(){
+			//GET SUBJECT INTEREST
 			var subject = $(this).attr("data-name");
+			//DEFINE APIKEY AND URL TO SEARCH
 			var APIKey = "QSE9U2nEeYIyI2u6ycZObppAgfOV8mLD";
 			var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + subject + "&api_key=" + APIKey + "&limit=10";
+			//AJAX CALL
 			$.ajax({
 				url: queryURL,
 				method: "GET"
 			})
+			//WHEN DONE CALLING AJAX
 			.done(function(response){
-				console.log(response);
+				//DEFINE A VARIABLE TO CAPTURE ALL RETRIEVED DATA
 				var results = response.data;
+				//DISPLAY THE DATA USING A FOR LOOP THAT TRAVERSES THE RESPONSE DATA
 				for (var i = 0; i < 10; i++){
 					var contentDiv = $("<div>");
 					contentDiv.addClass("content");
@@ -41,24 +47,34 @@
 				}
 			});
 		});
+		//FUNCTION TO DISPLAY THE BUTTONS
 		function displayButtons(){
+			//EMPTY BUTTON SECTION TO RESPAWN THE BUTTONS
 			$("#buttonsGoHere").empty();
+			//USE A FOR LOOP TO TRAVERSE THE BUTTON ARRAY AND DISPLAY
 			for (var i = 0; i < topics.length; i++){
+				//CREATE A BUTTON ELEMENT
 				var button = $("<button>");
 				button.addClass("subject");
 				button.attr("data-name", topics[i]);
+				//ADD THE TEXT INTO THE BUTTON
 				button.text(topics[i]);
+				//DISPLAY THE BUTTON
 				$("#buttonsGoHere").append(button);
 			}
 		};
+		//UPON CLICKING A GIF ANIMATE IT, OR MAKE IT STAND STILL
 		$("#gifsGoHere").on("click", ".chosenGif", function(){
+			//CHECK THE STATE OF THE GIF
 			var state = $(this).attr("data-state");
 			var animated = $(this).attr("data-animate");
 			var notAnimated = $(this).attr("data-still");
+			//IF NOT ANIMATED, ANIMATE THE GIF
 			if (state === "still"){
 				$(this).attr("data-state", "animate");
 				$(this).attr("src", animated);
 			}
+			//IF ANIMATED, MAKE THE GIF STAY STILL
 			if (state === "animate"){
 				$(this).attr("data-state", "still");
 				$(this).attr("src", notAnimated);
