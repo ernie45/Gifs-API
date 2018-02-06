12120f579eedb7effb0a55f0cc7e033dc761992c
Ernesto Hernandez <h_ernesto@ymail.com>
2018-02-06 01:13:21 -0800

    Restructured

--- assets/javascript/gifAPI.js
+++ assets/javascript/gifAPI.js
@@ -1,68 +1,27 @@
+//ORIGINAL BUTTONS
+var topics = ["leonardo", "donatello", "michaelangelo", "rafael", "splinter"];
+
+
+
 //MAIN FUNCTION////////////////////////////////////////////////////////////////////////////////////////////////////		
 	$(document).ready(function(){
-		//ORIGINAL BUTTONS
-		var topics = ["pozole", "tamales", "enchiladas", "sopes", "chile con carne", "birria"];
 		//SHOW THE BUTTONS
 		displayButtons();
 		//ADD A TOPIC AND APPEND TO THE BUTTONS LIST
 		$("#add-topic").on("click", function(event){
 			event.preventDefault();
-			var subject = $("#gif-input").val().trim();
-			topics.push(subject);
-			displayButtons();
+			/** Check if the input has any value */
+			if ($("#gif-input").val().trim().length){
+				/** If there is content in the input, add that topic as a button */
+				addTopic();
+			}
 		});
 		//CLICKING ON A BUTTON SEARCHES FOR THAT SUBJECT
 		$("#buttonsGoHere").on("click", ".subject", function(){
 			//GET SUBJECT INTEREST
 			var subject = $(this).attr("data-name");
-			//DEFINE APIKEY AND URL TO SEARCH
-			var APIKey = "QSE9U2nEeYIyI2u6ycZObppAgfOV8mLD";
-			var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + subject + "&api_key=" + APIKey + "&limit=10";
-			//AJAX CALL
-			$.ajax({
-				url: queryURL,
-				method: "GET"
-			})
-			//WHEN DONE CALLING AJAX
-			.done(function(response){
-				//DEFINE A VARIABLE TO CAPTURE ALL RETRIEVED DATA
-				var results = response.data;
-				//DISPLAY THE DATA USING A FOR LOOP THAT TRAVERSES THE RESPONSE DATA
-				for (var i = 0; i < 10; i++){
-					var contentDiv = $("<div>");
-					contentDiv.addClass("content");
-					var imgDiv = $("<img>");
-					imgDiv.addClass("chosenGif");
-					imgDiv.attr("src", results[i].images.fixed_height_still.url);
-					imgDiv.attr("data-still", results[i].images.fixed_height_still.url);
-					imgDiv.attr("data-animate", results[i].images.fixed_height.url);
-					imgDiv.attr("data-state", "still");
-					imgDiv.attr("")
-					var ratingDiv = $("<div>");
-					ratingDiv.addClass("rating");
-					ratingDiv.text("image rating: " + results[i].rating);
-					contentDiv.append(imgDiv);
-					contentDiv.append(ratingDiv);
-					$("#gifsGoHere").prepend(contentDiv);
-				}
-			});
+			makeAjaxCallForSubject(subject);
 		});
-		//FUNCTION TO DISPLAY THE BUTTONS
-		function displayButtons(){
-			//EMPTY BUTTON SECTION TO RESPAWN THE BUTTONS
-			$("#buttonsGoHere").empty();
-			//USE A FOR LOOP TO TRAVERSE THE BUTTON ARRAY AND DISPLAY
-			for (var i = 0; i < topics.length; i++){
-				//CREATE A BUTTON ELEMENT
-				var button = $("<button>");
-				button.addClass("subject");
-				button.attr("data-name", topics[i]);
-				//ADD THE TEXT INTO THE BUTTON
-				button.text(topics[i]);
-				//DISPLAY THE BUTTON
-				$("#buttonsGoHere").append(button);
-			}
-		};
 		//UPON CLICKING A GIF ANIMATE IT, OR MAKE IT STAND STILL
 		$("#gifsGoHere").on("click", ".chosenGif", function(){
 			//CHECK THE STATE OF THE GIF
@@ -81,4 +40,67 @@
 			}
 		});
 	});
-		
+
+
+
+	//FUNCTIONS TO CALL///////////////////////////////////////////////////////////////////////////////////////////////
+	function addTopic(){
+		var subject = $("#gif-input").val().trim();
+			/** Push the subject to the topics */
+			topics.push(subject);
+			/** Refresh the buttons */
+			displayButtons();
+	};
+	//FUNCTION TO DISPLAY THE BUTTONS
+	function displayButtons(){
+		/** Empty buttons so they won't add up */
+		$("#buttonsGoHere").empty();
+		//USE A FOR LOOP TO TRAVERSE THE BUTTON ARRAY AND DISPLAY
+		for (var i = 0; i < topics.length; i++){
+			/** Define a new button with the class subject, a data attribute and display the subject */
+			var button = $("<button>").addClass("subject").attr("data-name", topics[i]).text(topics[i]);
+			//DISPLAY THE BUTTON
+			$("#buttonsGoHere").append(button);
+		}
+	};
+	function makeAjaxCallForSubject(subject){
+		//DEFINE APIKEY AND URL TO SEARCH
+		var APIKey = "QSE9U2nEeYIyI2u6ycZObppAgfOV8mLD";
+		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + subject + "&api_key=" + APIKey + "&limit=10";
+
+
+		//AJAX CALL
+		$.ajax({
+			url: queryURL,
+			method: "GET"
+		})
+		//WHEN DONE CALLING AJAX
+		.done(function(response){
+			//DEFINE A VARIABLE TO CAPTURE ALL RETRIEVED DATA
+			var results = response.data;
+			//DISPLAY THE DATA USING A FOR LOOP THAT TRAVERSES THE RESPONSE DATA
+			createGifContent(results);
+		});
+	};
+	function createGifContent(results){
+		/** For only 10 elements */
+		for (var i = 0; i < 10; i++){
+			/** Define a div with a content class */
+			var contentDiv = $("<div>").addClass("content");
+			/** Define a div to hold the gifs */
+			var imgDiv = $("<img>")
+				  .addClass("chosenGif")
+				  .attr("src", results[i].images.fixed_height_still.url)
+				  .attr("data-still", results[i].images.fixed_height_still.url)
+				  .attr("data-animate", results[i].images.fixed_height.url)
+				  .attr("data-state", "still");
+			/** Define a div to hold the rating of the image */
+			var ratingDiv = $("<div>")
+					 .addClass("rating")
+					 .text("image rating: " + results[i].rating);
+			/** To the main content div, add the image div and the rating div */
+			contentDiv.append(imgDiv).append(ratingDiv);
+			/** Display the contentDiv that holds the gifs */
+			$("#gifsGoHere").prepend(contentDiv);
+		}
+	};
